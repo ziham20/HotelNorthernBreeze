45c38453d92c9e67de8d75d0df54bf90307e2584
Ziham Zuhair <ziham.zuhair@qualitapps.com>
2021-08-14 01:52:38 +0530

    UI changes

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 #nullable disable
 
@@ -7,12 +8,16 @@ namespace HotelNorthernBreeze.Models
 {
     public partial class Booking
     {
+       
         public string Category { get; set; }
         public string Size { get; set; }
-        public string UserNic { get; set; }
-        public DateTime FromDate { get; set; }
-        public DateTime ToDate { get; set; }
 
+        public string UserNic { get; set; }
+
+        [DisplayName("Check-in")]
+        public DateTime FromDate { get; set; }
+        [DisplayName("Check-out")]
+        public DateTime ToDate { get; set; }
         public virtual Room Room { get; set; }
         public virtual User UserNicNavigation { get; set; }
     }
