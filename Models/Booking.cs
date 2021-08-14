0a9ec26e5aa2310ac4e565e63fbc20618226bbeb
Lasindu <lasindunuwanga@gmail.com>
2021-08-14 12:02:36 +0530

    Model validations were added

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,16 +9,28 @@ namespace HotelNorthernBreeze.Models
 {
     public partial class Booking
     {
-       
+
+        [Required]
+        [DataType(DataType.Text)]
         public string Category { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         public string Size { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [RegularExpression("^([0-9]{9}[x|X|v|V]|[0-9]{12})$", ErrorMessage = "Invalid NIC")]
         public string UserNic { get; set; }
 
+        [DataType(DataType.Date)]
         [DisplayName("Check-in")]
         public DateTime FromDate { get; set; }
+
+        [DataType(DataType.Date)]
         [DisplayName("Check-out")]
         public DateTime ToDate { get; set; }
+
         public virtual Room Room { get; set; }
         public virtual User UserNicNavigation { get; set; }
     }
